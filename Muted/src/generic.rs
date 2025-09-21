38f45f5d422b225a0adc7b8b6bebc9b520ab9eda
arquitenens <norodnoastro@gmail.com>
2025-09-21 11:03:53 +0200

    Used NonNull because uhm its non null? also its safes a bit of space and is probably more idomatic.. not that i would care about that though

--- Muted/src/generic.rs
+++ Muted/src/generic.rs
@@ -3,11 +3,12 @@ use std::fmt::{Debug, Display, Formatter};
 use std::hash::Hash;
 use std::mem::ManuallyDrop;
 use std::ptr;
+use std::ptr::NonNull;
 
 #[derive(Debug, Eq, Hash, PartialEq)]
 pub enum Data<T: Eq + Hash> {
     Val(T),
-    Rp(Option<*mut Vec<Data<T>>>),
+    Rp(Option<NonNull<Vec<Data<T>>>>),
 }
 #[derive(Debug, Eq, PartialEq)]
 pub struct Muted<T: Hash + Eq + Debug> {
@@ -32,7 +33,7 @@ impl<T: Eq + Hash + Debug + Clone> Iterator for Muted<T>{
 
 impl<T: Eq + Hash + Debug> Drop for Muted<T>{
     fn drop(&mut self) {
-        let mut to_drop: Vec<(*mut Vec<Data<T>>, usize)> = Vec::new();
+        let mut to_drop: Vec<(NonNull<Vec<Data<T>>>, usize)> = Vec::new();
         for (idx, item) in self.data.iter_mut().enumerate() {
             if let Data::Rp(opt_ptr) = item {
                 if let Some(ptr) = opt_ptr.take() {
@@ -62,7 +63,7 @@ impl<T: Eq + Hash + Debug> Display for Muted<T>{
                     Data::Val(v) => display_vec.push(v),
                     Data::Rp(p1) => if let Some(p) = p1{
                         unsafe {
-                            let vec: &Vec<Data<T>> = &**p;
+                            let vec: &Vec<Data<T>> = p.as_ref();
                             for i in vec.iter(){
                                 match i {
                                     Data::Val(v) => display_vec.push(v),
@@ -88,7 +89,7 @@ impl<T: Hash + Eq + Debug> Muted<T>{
             data: Box::new(Self::muted_from(vec)),
             r_hold: HashMap::new(),
             prefix_vec: ((1..=len).collect(), len),
-            rc: 0 
+            rc: 0
         };
     }
     
@@ -111,8 +112,8 @@ impl<T: Hash + Eq + Debug> Muted<T>{
     pub fn push_vec(&mut self, other: Vec<Data<T>>) {
         let len = other.len();
         let mut other = ManuallyDrop::new(Box::new(other));
-        let ptr: *mut Vec<Data<T>> = &mut **other;
-        let maybe_ptr = Some(ptr);
+        let ptr: &mut Vec<Data<T>> = &mut **other;
+        let maybe_ptr = NonNull::new(ptr);
         self.r_hold.insert(Data::Rp(maybe_ptr), Some((other, self.data.len(), len)));
         self.data.push(Data::Rp(maybe_ptr));
         self.rc += 1;
@@ -127,7 +128,8 @@ impl<T: Hash + Eq + Debug> Muted<T>{
     pub fn len(&self) -> usize {
         *self.prefix_vec.0.last().unwrap()
     }
-    pub fn drop_vec(&mut self, index: Option<usize>, maybe_ptr: Option<*mut Vec<Data<T>>>) -> Option<()>{
+
+    pub fn drop_vec(&mut self, index: Option<usize>, maybe_ptr: Option<NonNull<Vec<Data<T>>>>) -> Option<()>{
         match (index, maybe_ptr) {
             (Some(_),Some(_)) => {
                 panic!("choose either index or direct pointer");
@@ -170,7 +172,7 @@ impl<T: Hash + Eq + Debug> Muted<T>{
                 
                 std::mem::drop(boxed);
                 self.rc -= 1;
-            }else { 
+            }else {
                 return None;
             }
             self.data[real_index.unwrap()] = Data::Rp(None);
@@ -179,21 +181,24 @@ impl<T: Hash + Eq + Debug> Muted<T>{
             return None;
         }
     }
+
     pub unsafe fn insert_vec_unchecked(&mut self, index: usize, other: Vec<T>) -> Option<()>{
         self.insert_vec_inner(index, other, true)
     }
+
     pub fn insert_vec(&mut self, index: usize, other: Vec<T>) -> Option<()>{
         if index >= self.data.len() { return None; }
         self.insert_vec_inner(index, other, false)
     }
+
     fn insert_vec_inner(&mut self, index: usize, vec: Vec<T>, skip_calibration: bool) -> Option<()>{
         let len = vec.len();
         let wrapped: Vec<Data<T>> = vec.into_iter().map(Data::Val).collect();
         let mut other = ManuallyDrop::new(Box::new(wrapped));
-        let ptr: *mut Vec<Data<T>> = &mut **other;
+        let ptr= NonNull::new(&mut **other);
         if self.data[index] == Data::Rp(None) {
-            self.r_hold.insert(Data::Rp(Some(ptr)), Some((other, index, len)));
-            self.data[index] = Data::Rp(Some(ptr));
+            self.r_hold.insert(Data::Rp(ptr), Some((other, index, len)));
+            self.data[index] = Data::Rp(ptr);
         }else {
             {
                 ManuallyDrop::into_inner(other);
@@ -236,7 +241,7 @@ impl<T: Hash + Eq + Debug> Muted<T>{
             Data::Rp(p) => {
                 if let Some(pointer) = p{
                     unsafe {
-                        let vec: &mut Vec<Data<T>> = &mut **pointer;
+                        let vec: &mut Vec<Data<T>> = &mut *pointer.as_ptr();
                         let offset = if rough_index == 0{
                             index
                         }else {
@@ -256,7 +261,7 @@ impl<T: Hash + Eq + Debug> Muted<T>{
 
         };
     }
-    
+
     unsafe fn calibrate_index(&mut self, added_len: usize){
         let added_len = added_len as usize;
         let start = self.prefix_vec.1;
